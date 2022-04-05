0a33dd938acfc3d9aa5cd826799f59d0b6594359
kevinksit <kevinkamansit@gmail.com>
2022-04-04 17:49:45 -0700

    updating some of the helper classes

--- _Helpers/DataStructure.m
+++ _Helpers/DataStructure.m
@@ -11,17 +11,18 @@ classdef DataStructure < handle
 			obj.stimulus_data = stimulus_data;
 
 			if sum(obj.stimulus_data.heading > 200) > 1
-				error('Heading > 200, what''s wrong here?')
+				error('Heading has gone over 200... something is fundamentally wrong here')
 			end
-			obj.checkForSpikes();
+			obj.checkForSpikes(); % See if spikes have been inferred
 		end
 
 		function out = get(obj, field)
-			if isfield(obj.neural_data, field) && ~isfield(obj.stimulus_data, field)				
+			% just a quick way of getting variables out
+			if isfield(obj.neural_data, field) && ~isfield(obj.stimulus_data, field) % field only in neural data
 				out = obj.neural_data.(field);
-			elseif isfield(obj.stimulus_data, field) && ~isfield(obj.neural_data, field)				
+			elseif isfield(obj.stimulus_data, field) && ~isfield(obj.neural_data, field) % field only in stimulus data				
 				out = obj.stimulus_data.(field);
-			elseif isfield(obj.neural_data, field) && isfield(obj.stimulus_data, field)
+			elseif isfield(obj.neural_data, field) && isfield(obj.stimulus_data, field) % field in both, should really never happen..
 				p = obj.resolveOverlappingFields()
 				out = obj.(p).(field);
 			else
@@ -30,6 +31,7 @@ classdef DataStructure < handle
 		end
 
 		function set(obj, field, value)
+			% for setting fields, not commonly used because we don't want to edit the data in the fields
 			if isfield(obj.neural_data, field) && ~isfield(obj.stimulus_data, field)
 				obj.neural_data.(field) = value
 			elseif isfield(obj.stimulus_data, field) && ~isfield(obj.neural_data, field)
@@ -43,25 +45,25 @@ classdef DataStructure < handle
 		end
 
 		function out = getNeuralData(obj)
-			out = obj.neural_data;
+			out = obj.neural_data; % return neural data structure
 		end
 
 		function out = getStimulusData(obj)
-			out = obj.stimulus_data;
+			out = obj.stimulus_data; % return stimulus data structure
 		end
 
 		function save(obj)
 			new_filename = obj.neural_data.filename;
 			new_filename = new_filename(1:end-4);
 			neural_data = obj.neural_data;
-            try
-                save(strcat(new_filename, '_data.mat'), 'neural_data')
-            catch
-                fullfile = pwd;
-                backslashes = strfind(fullfile, '\');
-                new_filename = strcat(fullfile(backslashes(end) + 1:end));
-                save(strcat(new_filename, '_data.mat'), 'neural_data')
-            end
+			try
+				obj.saveNeuralData();
+			catch
+				fullfile = pwd;
+				backslashes = strfind(fullfile, '\');
+				new_filename = strcat(fullfile(backslashes(end) + 1:end));
+				save(strcat(new_filename, '_data.mat'), 'neural_data')
+			end
 
 			stimdat_filename = sprintf('%s_stimulus_data.mat', date);
 			stimulus_data = obj.stimulus_data;
@@ -74,30 +76,31 @@ classdef DataStructure < handle
 			save(strcat(new_filename, '_data.mat'), 'data')
 		end
 	end
-	
+
 	methods (Access = protected)
 		function out = resolveOverlappingFields(obj)
 			current_properties = properties(obj);
-			out = questdlg('Choose the data you want to modify: ', 'Modify', current_properties{:}, 'neural_data');
-		end
+				out = questdlg('Choose the data you want to modify: ', 'Modify', current_properties{:}, 'neural_data');
+			end
 
-		function checkForSpikes(obj)
-			if ~isfield(obj.neural_data, 'spikes')
-				disp('Getting spikes')
-				obj.neural_data.spikes = obj.getSpikeData(obj.neural_data);
-				obj.save();
+			function checkForSpikes(obj)
+				if ~isfield(obj.neural_data, 'spikes')
+					disp('Getting spikes')
+					obj.neural_data.spikes = obj.getSpikeData(obj.neural_data);
+					obj.save();
+				end
+			end
+
+			function dffDeconv = getSpikeData(obj, data)
+				dffDeconv = zeros(size(data.DFF));
+				for n = 1:size(data.DFF, 1)
+					% get trace and run deconvolution
+					trace = data.DFF(n, :);
+					[~, spikes, ~] = deconvolveCa(trace, 'ar1' ,'foopsi', 'optimize_pars');
+					dffDeconv(n, :) = spikes;
+				end
+				close all;
 			end
 		end
-
-		function dffDeconv = getSpikeData(obj, data)
-			dffDeconv = zeros(size(data.DFF));
-			for n = 1:size(data.DFF, 1)
-                % get trace and run deconvolution
-                trace = data.DFF(n, :);
-                [~, spikes, ~] = deconvolveCa(trace, 'ar1' ,'foopsi', 'optimize_pars');
-                dffDeconv(n, :) = spikes;
-            end
-            close all;
-        end
-    end
+	end
 end
--- _Helpers/Separator.m
+++ _Helpers/Separator.m
@@ -12,29 +12,30 @@ classdef Separator < handle
 		function obj = Separator(data_structure, segment_length, n_repeats)
 			obj.data_structure = data_structure;
 			obj.segment_length = segment_length;
-			obj.recording_length = size(obj.data_structure.get('raw_F'), 2);
+			obj.recording_length = size(obj.data_structure.get('raw_F'), 2); % raw_F is a pretty safe field to pull for the recording length
 			obj.n_repeats = n_repeats;
 			obj.n_trials = floor(obj.recording_length/(obj.segment_length * obj.n_repeats));
 		end
 
 		function data = run(obj)
+			% Main look, separates both neural and stimulus data structures
 			neural_data = obj.data_structure.getNeuralData();
 			stimulus_data = obj.data_structure.getStimulusData();
 			for r = 1:obj.n_repeats
 				for t = 1:obj.n_trials
-					[n_d] =  obj.separateRecording(neural_data, r, t);
-					[s_d] =  obj.separateRecording(stimulus_data, r, t);
+					[n_d] =  obj.separateData(neural_data, r, t); % separate the neural data
+					[s_d] =  obj.separateData(stimulus_data, r, t); % separate the stimulus data
 					data{r, t} = DataStructure(n_d, s_d);
 				end
 			end
 		end
 
-		function out = separateRecording(obj, data, rep, trial)
+		function out = separateData(obj, data, rep, trial)
 			%% Get all the fields in the data structure
 			out = struct();
-			struct_fields = fields(data);
+			struct_fields = fields(data); % for each field
 			for i_field = 1:length(struct_fields)
-				if ismember(obj.recording_length, size(data.(struct_fields{i_field})))
+				if ismember(obj.recording_length, size(data.(struct_fields{i_field}))) % some fields shouldn't be separated... 
 					out.(struct_fields{i_field}) = obj.separateField(data.(struct_fields{i_field}), rep, trial); %this is to make sure we only separate the right fields
 				else
 					out.(struct_fields{i_field}) = data.(struct_fields{i_field});
@@ -42,7 +43,7 @@ classdef Separator < handle
 			end
 		end
 		function out = separateField(obj, data, rep, trial)
-			% For separating out a single data
+			% For separating out a single field
 			out = [];
 			curr = (rep - 1) * (obj.segment_length * obj.n_trials) + (trial - 1) * obj.segment_length;
 			% Prepare data, this is finding the correct index to segment
